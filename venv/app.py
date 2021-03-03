25edb69afb24ca6d590be2633ac32656af311d3d
Vidya <vzg805@gmail.com>
2021-03-03 11:20:05 -0600

    checking weather api/requests issue

--- venv/app.py
+++ venv/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import render_template
 
 from map_api import get_map_data # could be moved into a single apis.py file, but for now
-#from weather_api import get_weather_data # might be simpler to keep separate files for people to 
+from weather_api import get_weather_data # might be simpler to keep separate files for people to
                                      # work separately on
 app = Flask(__name__)
 
@@ -10,10 +10,10 @@ app = Flask(__name__)
 def index():
     # feel free to make structural changes to api call functions add parameters etc
     # as needed
-    #weather_data = get_weather_data()
+    weather_data = get_weather_data()
     map_data = get_map_data()
 
-    return render_template("index.html", weather_data=0, map_data=map_data)
+    return render_template("index.html", weather_data=weather_data, map_data=map_data)
 
 @app.route("/home")
 def home():
@@ -22,6 +22,6 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html")
-    
+
 if __name__ == '__main__':
     app.run(debug=True)
