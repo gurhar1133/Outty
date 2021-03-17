d6b6b6c42747ebc49420a95864b889993b751987
ZZanetti <zzanetti1993@gmail.com>
2021-03-16 20:12:50 -0600

    added simple signup form

--- venv/app.py
+++ venv/app.py
@@ -6,19 +6,16 @@ from weather_api import get_weather_data # might be simpler to keep separate fil
 app = Flask(__name__)
 
 @app.route("/")
-def index(username=None):
+@app.route("/home")
+def index(userId=None):
     # feel free to make structural changes to api call functions add parameters etc
     # as needed
     weather_data = get_weather_data()
     map_data = get_map_data()
-    username = request.args.get("username")
-    if not username:
-        username="Explorer"
-    return render_template("index.html", weather_data=weather_data, map_data=map_data, username=username)
-
-@app.route("/home")
-def home():
-    return index()
+    userId = request.args.get("userId")
+    if not userId:
+        userId="Explorer"
+    return render_template("index.html", weather_data=weather_data, map_data=map_data, userId=userId)
 
 @app.route("/about")
 def about():
@@ -28,9 +25,39 @@ def about():
 def login():
     error = None
     if request.method == 'POST':
-        return redirect(url_for('index', username=request.form['username'])) #request.form['username']
+        return redirect(url_for('index',
+         userId=request.form['userId'],
+         password=request.form['password'],
+         )) #request.form['userId']
     else:
         return render_template('login.html', error=error)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    error = None
+    if request.method == 'POST':
+        """
+        write to database
+        password = request.form['password'],
+        emailAddress = request.form['emailAddress'],
+        userImage = request.form['userImage'],
+        hikes = request.form.get('hikes'),
+        mountainBikes = request.form.get['mountainBikes'],
+        roadBikes = request.form.get['roadBikes'],
+        camps = request.form.get['camps'],
+        userLocation = request.form['userLocation'],
+        """
+        return redirect(url_for('index',
+        userId=request.form['userId'],
+        ))
+         #request.form['userId']
+    else:
+        return render_template('signup.html', error=error)
+
+@app.route('/profile', methods=['GET', 'POST'])
+def profile():
+    #should read from the database to display info
+    return render_template('login.html', error=error)
 
 if __name__ == '__main__':
     app.run(debug=True)
