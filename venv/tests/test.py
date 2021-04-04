44c2f2735339b7c7da4591a98ff52556d0d02b19
Gurhar Khalsa <gurhar@Gurhars-MacBook-Pro.local>
2021-04-04 15:33:24 -0600

    tests and outty_database.db added to gitignore

--- venv/tests/test.py
+++ venv/tests/test.py
@@ -26,10 +26,40 @@ class OuttyTestCase(unittest.TestCase):
         self.assertEqual(True, True)
 
     def test_rec_construcor_input(self):
-        pass
+        test_rec = Recommender(None) ## should be flexible enough to handle Null input
+        recs = test_rec.recommend()[0]
+        self.assertEqual(type(recs), list)
+        # but if type of constructor input is a number or something 
+        # other than None or string then recommender should
+        # trigger an exception
+        try:
+            test_rec2 = Recommender(1)
+            self.fail("Should trigger exception when non string inputed")
+        except Exception:
+            
+            pass
+        try:
+            test_rec3 = Recommender([23,2])
+            self.fail("Should trigger exception when non string inputed")
+        except Exception:
+            pass
+        try:
+            test_rec4 = Recommender({"name":"id"})
+            self.fail("Should trigger exception when non string inputed")
+        except Exception:
+            print("cool, inputs handled correctly")
+            
+
+
+
+    def test_rec_on_bad_username(self):
+        test_rec = Recommender("aFakeNameNotInDb") ## if not in db, should just treat as default explorer
+        recs = test_rec.recommend()[0]
+        self.assertEqual(type(recs), list)
 
     def test_rec_handle_empty_api_return(self):
         pass
+
 
     def test_rec_slow_response(self):
         pass
@@ -38,7 +68,12 @@ class OuttyTestCase(unittest.TestCase):
         pass
 
     def test_rec_get_activities(self):
-        pass
+        # if there is an error in retrieving fav activities then use hiking
+        test_rec = Recommender("aFakeNameNotInDb")
+        test_rec.fav_activities = []
+        recs = test_rec.recommend()[0]
+        self.assertEqual(type(recs), list)
+        
 
     def test_rec_get_user_loc(self):
         pass
@@ -48,6 +83,7 @@ class OuttyTestCase(unittest.TestCase):
 
     def test_rec_method_inputs(self):
         pass
+        
 
 # Main: Run Test Cases
 if __name__ == '__main__':
