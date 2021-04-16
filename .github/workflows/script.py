3344659873a1b7bf40eb197340fa561d4eb3023d
ZZanetti <52258185+ZZanetti@users.noreply.github.com>
2021-04-16 14:39:47 -0600

    Update script.py

--- .github/workflows/script.py
+++ .github/workflows/script.py
@@ -1,5 +1,13 @@
 import os
-trail_api_key = os.environ['TRAIL_API_KEY']
-geo_encode_key = os.environ['GEO_ENCODE_KEY']
-weather_api_key = os.environ['WEATHER_API_KEY']
-map_api_key = os.environ['MAP_API_KEY']
+
+config = f'''
+trail_api_key = "{os.environ['TRAIL_API_KEY']}"
+geo_encode_key = "{os.environ['GEO_ENCODE_KEY']}"
+weather_api_key = "{os.environ['WEATHER_API_KEY']}"
+map_api_key = "{os.environ['MAP_API_KEY']}"
+'''
+
+with open("venv/config.py", "w+") as f:
+  f.write(config)
+  
+  
