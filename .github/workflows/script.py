b67e521fb674351a9e48193fbc39f615c9b2d11f
ZZanetti <52258185+ZZanetti@users.noreply.github.com>
2021-04-16 14:30:04 -0600

    Create script.py

---
+++ .github/workflows/script.py
@@ -0,0 +1,5 @@
+import os
+trail_api_key = os.environ['TRAIL_API_KEY']
+geo_encode_key = os.environ['GEO_ENCODE_KEY']
+weather_api_key = os.environ['WEATHER_API_KEY']
+map_api_key = os.environ['MAP_API_KEY']
