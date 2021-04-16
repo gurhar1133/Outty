2ca6de9bdeb396d65bf28e49f10b100595e821ed
ZZanetti <zzanetti1993@gmail.com>
2021-04-16 11:08:31 -0600

    removed relative imports

--- venv/auth.py
+++ venv/auth.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, login_required
-from .models import User
-from . import db
-from .zipcodeCityState import get_citystate_data
+from models import User
+from __init__ import db
+from zipcodeCityState import get_citystate_data
 
 
 auth = Blueprint('auth', __name__)
