1d6561b6ff7139feb02ed9ae6e9f01b221296cc1
Gurhar Khalsa <gurhar@Gurhars-MacBook-Pro.local>
2021-04-18 13:26:12 -0600

    fixed empty recs and pipline of recs to dash

--- venv/recommend.py
+++ venv/recommend.py
@@ -106,6 +106,7 @@ class Recommender:
     def recommend(self):
 
         activity_pref = self.fav_activities
+        print("activity pref: ", activity_pref)
         postal_code = self.location
         if len(activity_pref) == 0:
             self.fav_activities = ['mountain biking']
@@ -142,9 +143,11 @@ class Recommender:
         lon = data['data'][0]['longitude']
 
         # makes a recommendation for each activity that the user likes
-        recs = [self.trail_api_query(lat, lon, state, act)
-                for act in activity_pref]
-
+        # recs = [self.trail_api_query(lat, lon, state, act)
+        #         for act in activity_pref]
+        recs = []
+        for act in activity_pref:
+            recs.append(self.trail_api_query(lat, lon, state, act))
         return recs
 
 
